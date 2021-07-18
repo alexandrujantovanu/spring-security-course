b3fcf87636fec1ae8f80061dc5e559d7a1fae637
Alexandru Jantovanu <alexandrujantovanu@gmail.com>
2021-07-18 15:38:58 +0300

    - added STUDENT user and ADMIN user - created ROLES and PERMISSIONS - restricted page access based on ROLES

---
+++ src/main/java/com/course/security/security/ApplicationUserRole.java
@@ -0,0 +1,22 @@
+package com.course.security.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.course.security.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+
+    private final Set<ApplicationUserPermission> permission;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permission) {
+        this.permission = permission;
+    }
+
+    public Set<ApplicationUserPermission> getPermission() {
+        return permission;
+    }
+}
